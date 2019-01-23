0d289007c488f7bf6d5cfd78c97e3900f406802a
Joel Bartlett <joel@murbar.com>
2019-01-23 15:21:28 -0500

    Complete 4 principles Qs

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. implicit binding -> 'this' refers to the object left of the 'dot' 
+* 2. explicit binding -> using the .call, .apply, or .bind methods to explicitly set context of 'this'
+* 3. the 'new' keyword -> when invoked, new object is returned -- 'this' in constructor function is bound to that object
+* 4. window binding -> when function is invoked without a 'dot' and not using .call, .apply, or .bind, 'this' will be bound to the window/global object -- "use strict" to prevent this undesirable behavior.
 *
 * write out a code example of each explanation above
 */
